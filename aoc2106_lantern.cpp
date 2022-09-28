8968ea3d472667d82626038327e9c088cfafb432
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-09-28 22:28:23 +0200

    ud. 2106 cleanup

--- aoc2106_lantern.cpp
+++ aoc2106_lantern.cpp
@@ -16,114 +16,116 @@
 #include "vector"
 #include "map"
 
-using   namespace std;
+using	namespace std;
 
-void    show_baseline(map<int, long> life);
-void    show_lifesize(map<int, long> life);
-long    mapsum(map<int, long>);
+void	show_baseline(map<int, long> life);
+void	show_lifesize(map<int, long> life);
+long	mapsum(map<int, long>);
 
 //      dp part 1 + 2
 
-int     main()
+int	main()
 {
-        map<int, long>  life;
-        string          s;
-        long            pop80, pop256;
-        int             diff, i, date, days;
+	map<int, long>		life;
+	string			s;
+	long			pop80, pop256;
+	int			diff, i, date, days;
 
-        cin >> s;
-        stringstream    ss(s);
-        while (getline(ss, s, ',')) life[stoi(s)]++;
-        show_lifesize(life);
-        show_baseline(life);
-        date = 0;
-        days = 256;
-        while (++date < days)
-        {
-            i = date % 9;
+	cin >> s;
+	stringstream	ss(s);
 
-            //  i is a weekday of a 9-day week
-            //  a new-born needs 9 days to become a breeder
-            //  ie. every 8 days
-            //  grown-ups need 7 days, ie. every 6 days
+	while (getline(ss, s, ','))
+		life[stoi(s)]++;
+	show_lifesize(life);
+	show_baseline(life);
+	date = 0;
+	days = 256;
+	while (++date < days)
+	{
+		i = date % 9;
 
-            diff = i < 2 ? i + 7 : i - 2;
-            life[diff] += life[i];
-            if (date + 1 == 80) pop80 = mapsum(life);
-        }
-        pop256 = mapsum(life);
+		//  i is a weekday of a 9-day week
+		//  a new-born needs 9 days to become a breeder
+		//  ie. every 8 days
+		//  grown-ups need 7 days, ie. every 6 days
 
-        cout << "Star 1: " << pop80 << endl;
-        cout << "Star 2: " << pop256 << endl;
-
-        return (0);
+		diff = i < 2 ? i + 7 : i - 2;
+		life[diff] += life[i];
+		if (date + 1 == 80)
+			pop80 = mapsum(life);
+	}
+	pop256 = mapsum(life);
+	cout << "Star 1: " << pop80 << endl;
+	cout << "Star 2: " << pop256 << endl;
 }
 
-long    mapsum(map<int, long> mp)
+long	mapsum(map<int, long> mp)
 {
-        long    size = 0;
-        int     i = -1;
+	long    size = 0;
+	int     i = -1;
 
-        while (++i < 9) size += mp[i];
-        return (size);
+	while (++i < 9)
+		size += mp[i];
+	return (size);
 }
 
-void    show_baseline(map<int, long> life)
+void	show_baseline(map<int, long> life)
 {
-        int     i = -1;
+	int     i = -1;
 
-        cout << "--\n";
-        while (++i < 9) cout << i << ":" << life[i] << "  ";
-        cout << "\n--" << endl;
+	cout << "--\n";
+	while (++i < 9)
+		cout << i << ":" << life[i] << "  ";
+	cout << "\n--" << endl;
 }
 
 void    show_lifesize(map<int, long> life)
 {
-        cout << "--" << endl;
-        cout << "lifesize: ";
-        cout << life.size() << endl;
+	cout << "--" << endl;
+	cout << "lifesize: ";
+	cout << life.size() << endl;
 }
 
 //      deprecated part 1
 
 /*
-
 void    debug(vector<int> vc);
 
 int     main()
 {
-        vector<int>     life;
-        string          s;
-        int             i, days = 80;
+	vector<int>     life;
+	string          s;
+	int             i;
+	int		days = 80;
 
-        cin >> s;
-        stringstream    ss(s);
-        while (getline(ss, s, ',')) life.push_back(stoi(s));
-        days++;
-        while (--days)
-        {
-            i = -1;
-            while (++i < life.size())
-            {
-                if (life[i])
-                    life[i]--;
-                else
-                {
-                    life[i] = 6;
-                    life.push_back(9);
-                }
-            }
-        }
-        cout << "Star 1: " << life.size() << endl;
+	cin >> s;
+	stringstream    ss(s);
 
-        return (O);
+	while (getline(ss, s, ','))
+		life.push_back(stoi(s));
+	days++;
+	while (--days)
+	{
+		i = -1;
+		while (++i < life.size())
+		{
+			if (life[i])
+				life[i]--;
+			else
+			{
+				life[i] = 6;
+				life.push_back(9);
+			}
+		}
+	}
+	cout << "Star 1: " << life.size() << endl;
 }
-
-void    debug(vector<int> vc)
-{
-        int     i = -1;
-
-        while (++i < vc.size()) cout << vc[i] << " ";
-}
-
 */
+
+void	debug(vector<int> vc)
+{
+	int	i = -1;
+
+	while (++i < vc.size())
+		cout << vc[i] << " ";
+}
