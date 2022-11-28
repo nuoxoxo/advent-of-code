0835d63dbdb9ae5b124fdf05d52df4f39720ad9e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-11-28 12:39:15 +0100

    edit 1802

--- aoc1802.cc
+++ aoc1802.cc
@@ -9,9 +9,13 @@ int	main()
 	int	count;
 	int	r2, r3;
 
+	vector<string>	v;
+
 	r2 = r3 = 0;
 	while (cin >> s)
 	{
+		v.push_back(s);
+
 		ok2 = 0;
 		ok3 = 0;
 		l = 'a' - 1;
@@ -51,6 +55,44 @@ int	main()
 			}
 		}
 	}
-	//cout << r2 << '*' <<  r3 << endl;
+
+	bool	ok;
+	int	i, j, k, idx;
+	string	part2;
+
+
+	i = -1;
+	while (++i < (int) v.size() - 1)
+	{
+		j = i;
+		while (++j < (int) v.size())
+		{
+			k = 0;
+			count = 0;
+			ok = false;
+			while (k < (int) v[i].length())
+			{
+				if (v[i][k] != v[j][k])
+				{
+					count++;
+					idx = k;
+				}
+				++k;
+			}
+			if (count ^ 1)
+				continue ;
+			ok = true;
+			part2 = v[i].substr(0, idx);
+			part2 += v[i].substr(idx + 1, (int) v[i].length() - 1 - idx);
+			break ;
+		}
+		if (ok)
+			break ;
+
+	}
+
+	// cout << r2 << '*' <<  r3 << endl; // part 1 debugger
+
 	cout << "Star 1: " << r2 * r3 << endl;
+	cout << "Star 2: " << part2 << endl;
 }
