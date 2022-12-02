636ee63e3ff9d5039e4cb3baf9aeee077f045f24
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-02 07:52:59 +0100

    fi. day 2 . brute forced

--- aoc2202a.cc
+++ aoc2202a.cc
@@ -1,58 +1,73 @@
 #include "iostream"
 #include "sstream"
 #include "map"
+#include "cctype"
 
 using	namespace std;
+
+/*
+a b c
+x y z
+
+R P S
+
+0 3 6
+1 2 3
+*/
 
 int	main()
 {
 	string	 	s;
 	int		res = 0;
-	map<char, int>	m{{'A',1}, {'B',2}, {'C',3}, {'X', 1}, {'Y', 2}, {'Z', 3}};
+	char 		a = 'a', b = 'b', c = 'c';
+	map<char, int>	m{{a,1}, {b,2}, {c,3}};
 
 	int		res2 = 0;
 
 	while (getline(cin, s))
 	{
 		stringstream	ss(s);
-		char		L, R;
-		int		l, r, rr;
+		char		l, r, rr;
 
-		ss >> L >> R;
-		l = m[L];
-		r = m[R];
+		ss >> l >> r;
+		l = tolower(l);
+		r = tolower(r) - ('x' - 'a');
 
 		//	part 1
 
-		if (l == r)
-			res += r + 3;
-		else
+		if (l == r) // draw
 		{
-			res += r;
+			res += m[r] + 3;
+		}
+		else // not draw
+		{
+			res += m[r];
 			rr = l + 1;
-			if (rr > 3)
-				rr = 1;
+			if (rr > c)
+				rr = a;
 			if (rr == r)
 				res += 6;
 		}
 
 		//	part 2
 
-		if (r == 2)
-			res2 += l + 3;
-		else if (r == 1)
+		if (r == b) // draw
+		{
+			res2 += m[l] + 3;
+		}
+		else if (r == a) // l;ose
 		{
 			--l;
-			if (l < 1)
-				l = 3;
-			res2 += l;
+			if (l < a)
+				l = c;
+			res2 += m[l];
 		}
-		else // assert 3
+		else if (r == c) // win
 		{
 			++l;
-			if (l > 3)
-				l = 1;
-			res2 += l + 6;
+			if (l > c)
+				l = a;
+			res2 += m[l] + 6;
 		}
 	}
 	cout << "Star 1: " << res << endl;
