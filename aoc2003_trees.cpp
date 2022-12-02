292550943cac262a862a630f25fd0e2ee4e77f8e
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-02 20:19:28 +0100

    cleanup

--- aoc2003_trees.cpp
+++ aoc2003_trees.cpp
@@ -11,7 +11,6 @@
 /*                                                                            */
 /* ****************************************************************** nuo *** */
 
-
 #include "iostream"
 #include "vector"
 #include "string"
@@ -22,34 +21,38 @@ int     tree(std::vector< std::string > x, int r, int d);
 
 int     main()
 {
-        std::vector< std::string >  x;
-        std::string s;
-        int         slope, slopes;
+	std::vector< std::string >  x;
+	std::string s;
+	
+	int         slope, slopes;
 
-        while (std::cin >> s)   x.push_back(s);
-        slope = tree(x, 3, 1);
-        slopes = slope;
-        slopes *= tree(x, 1, 1) * tree(x, 5, 1) * tree(x, 7, 1) * tree(x, 1, 2);
-        std::cout << "Star 1 : " << slope << std::endl;
-        std::cout << "Star 2 : " << slopes << std::endl;
-        
-        return (0);
+	while (std::cin >> s)
+		x.push_back(s);
+	slope = tree(x, 3, 1);
+	slopes = slope;
+	slopes *= tree(x, 1, 1) * tree(x, 5, 1) * tree(x, 7, 1) * tree(x, 1, 2);
+	std::cout << "Star 1 : " << slope << std::endl;
+	std::cout << "Star 2 : " << slopes << std::endl;
+
+	return (0);
 }
 
 //
 
 int     tree(std::vector< std::string > x, int r, int d)
 {
-        int     trees, i, j;
+	int     trees, i, j;
 
-        trees = i = j = 0;
-        while (i < (int) x.size())
-        {
-            if (x[i][j] == '#') trees++;
-            if (j + r < (int)   x[i].length()) j += r;
-            else    j = (j + r) % (int) x[i].length();
-            i += d;
-        }
-        
-        return (trees);
+	trees = i = j = 0;
+	while (i < (int) x.size())
+	{
+		if (x[i][j] == '#')
+			trees++;
+		if (j + r < (int) x[i].length())
+			j += r;
+		else
+			j = (j + r) % (int) x[i].length();
+		i += d;
+	}
+	return (trees);
 }
