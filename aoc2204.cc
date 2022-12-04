f36a7911cd6a5d21141c4345815c4735236e54ed
nuo <49533950+nuoxoxo@users.noreply.github.com>
2022-12-04 06:30:33 +0100

    leetcode math . fi. day 4

--- aoc2204.cc
+++ aoc2204.cc
@@ -1,13 +1,25 @@
-#include "header.hpp"
+#include "h.hpp"
+#include "cstdio"
 
 int	main()
 {
 	vector<string>	a;
 	string		s;
+	int	l,r,ll,rr, res = 0, r2 = 0;
 
 	while (cin >> s)
 	{
-		cout << s << endl;
-		a.push_back(s);
+		sscanf(s.c_str(),"%d-%d,%d-%d",&l,&r,&ll,&rr);
+		if ((l <= ll && r >= rr) || (ll <= l && rr >= r))
+			res++;		
+		//if ( (r >= ll && rr > r) || (ll <= r && l > ll)) // buggy
+		if (r >= ll && l <= rr)
+		{
+			// cout << s << endl;
+			r2++;
+		}
+		
 	}
+	cout << res << endl;
+	cout << r2 << endl;
 }
