2c0e07822bb8a1fb3071077289f599db2f389ff0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-26 01:39:27 +0100

    + 2015 day 7 . fix

--- aoc1507.py
+++ aoc1507.py
@@ -41,49 +41,9 @@ def solve (part2=False):
                 del CMD[i]
             except KeyError:
                 continue
-    return D
-    if not part2:
-        return D
-    CMD = lines.copy()
-    part1 = D['a']
-    D = {}
-    while CMD:
-        for i, line in enumerate(CMD):
-            # print(expr, val)
-            def getter (val):
-                if val.isdigit():
-                    return int(val)
-                if val == 'b':
-                    return part1
-                return D[val]
-            def parser (expr):
-                EXP = expr.split()
-                if len(EXP) == 1:
-                    return getter(EXP[0])
-                if EXP[0] == 'NOT':
-                    val = getter(EXP[1])
-                    return SIG ^ val
-                if len(EXP) > 3:
-                    assert False
-                L, op, R = EXP
-                L, R = getter(L), getter(R)
-                if op == 'LSHIFT':
-                    return L << R
-                if op == 'RSHIFT':
-                    return L >> R
-                if op == 'AND':
-                    return L & R
-                if op == 'OR':
-                    return L | R
-                assert False
-            expr, val = line.split(' -> ')
-            #print(val, expr)
-            try:
-                D[val] = parser(expr)
-                del CMD[i]
-            except KeyError:
-                continue
-    return D
-print('part 1:', solve()['a'])
-print('part 2:', solve( solve()['a'] )['a'])
+    return D['a']
+
+part1 = solve()
+print('part 1:', part1)
+print('part 2:', solve( part1 ))
 
